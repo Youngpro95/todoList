1e3143a3900c8865825257c1445329ee4e409a67
Youngpro95 <mmyoung95@naver.com>
2022-08-22 21:23:50 +0900

    feat : 실시간 시간 보여주기 기능 추가

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import TodoInfoTime from "./TodoInfoTime";
 import TodoTemplateWindow from "./TodoTemplate";
+import styled from "styled-components"
+import TodoInput from "./TodoInput";
+
 
 const App = () => {
   return (
--- todo/src/TodoInfoTime.js
+++ todo/src/TodoInfoTime.js
@@ -4,19 +4,32 @@ import { useInterval } from "use-interval";
 
 const TodoInfoBlock = styled.div`
   border-bottom: 1px solid #e9ecef;
-  font-size : 30px;
+  font-size: 30px;
   text-align: center;
 `;
 
-const calTime= ()=> {
+const TitleTodo = styled.div`
+  text-align: center;
+  font-size: 50px;
+  padding-top : 20px;
+`;
+
+const calTime = () => {
   const date = new Date();
-  const arrDayStr=['일','월','화','수','목','금','토'];
-  const day =(date.getMonth()+1)+'월 '+date.getDate()+'일 ('+arrDayStr[date.getDay()]+')';
+  const arrDayStr = ["일", "월", "화", "수", "목", "금", "토"];
+  const day =
+    date.getMonth() +
+    1 +
+    "월 " +
+    date.getDate() +
+    "일 (" +
+    arrDayStr[date.getDay()] +
+    ")";
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   return `${day} ${hours}:${minutes}:${seconds}`;
-}
+};
 
 const TodoInfoTime = () => {
   const [realTime, setRealTime] = useState(calTime());
@@ -24,7 +37,12 @@ const TodoInfoTime = () => {
   useInterval(() => {
     setRealTime(calTime());
   }, 1000);
-  return <TodoInfoBlock>{realTime}</TodoInfoBlock>;
+  return (
+    <>
+      <TodoInfoBlock>{realTime}</TodoInfoBlock>
+      <TitleTodo>TO-DO</TitleTodo>
+    </>
+  );
 };
 
 export default TodoInfoTime;
